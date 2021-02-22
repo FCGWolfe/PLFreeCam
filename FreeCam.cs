861f5969f9ec187bf4da2271061456c75d29acd7
Hunter Wolfe <fcgwolfe@gmail.com>
2021-02-22 15:59:05 -0800

    Add project files.

---
+++ FreeCam.cs
@@ -0,0 +1,41 @@
+﻿using PulsarPluginLoader.Chat.Commands;
+
+namespace PLFreeCam
+{
+	public class FreeCam : IChatCommand
+	{
+		public string[] CommandAliases()
+		{
+			return new string[]
+			{
+				"freecam"
+			};
+		}
+
+		public string Description()
+		{
+			return "PULSAR Freecam Plugin!";
+		}
+
+		public string UsageExample()
+		{
+			return "/" + this.CommandAliases()[0];
+		}
+		public bool PublicCommand()
+		{
+			return false;
+		}
+		public bool Execute(string arguments, int executor)
+		{
+			if (PLCameraSystem.Instance.CurrentCameraMode != null && PLCameraSystem.Instance.CurrentCameraMode.GetType() == typeof(PLCameraMode_Scripted))
+			{
+				PLCameraSystem.Instance.ChangeCameraMode(new PLCameraMode_LocalPawn());
+			}
+			else
+			{
+				PLCameraSystem.Instance.ChangeCameraMode(new PLCameraMode_Scripted());
+			}
+			return false;
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using PulsarPluginLoader;
+
+
+namespace PLFreeCam
+{
+    class Plugin : PulsarPlugin
+    {
+        public override string HarmonyIdentifier()
+        {
+            return "PLFreecam";
+        }
+
+        public override string Name => "PLFreecam";
+
+        public override string Version => "1.0b";
+
+        public override string Author => "FCGWolfe/Ship's Compooter";
+
+        public override string ShortDescription => "Gives you an out-of-body-experience.";
+    }
+}
+
